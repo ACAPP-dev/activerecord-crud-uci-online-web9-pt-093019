3827c265a8088d0c70132dbeda327ae10355a3a2
Andrew Capp <acapp909@gmail.com>
2020-01-02 23:45:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -53,7 +53,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
@@ -61,6 +61,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
+  Movie.find_by(:title == "Title", :release_date == 2000, :director == "Me")
 
 end
 
