8b17586a59adca997a6ab325b7bb6a64bb87490a
Andrew Capp <acapp909@gmail.com>
2020-01-02 23:39:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,10 +34,10 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  m = Movie.new
-  m.save
-  m
-
+  Movie.create do |m|
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+  end
 end
 
 def can_get_the_first_item_in_the_database
