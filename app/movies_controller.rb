c8408fc8c01d5e72ff799fc2e3d8fa38bfce2a2c
Andrew Capp <acapp909@gmail.com>
2020-01-02 23:34:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-
+  m = Movie.new(attributes)
+  m.save
+  m
 
 end
 
