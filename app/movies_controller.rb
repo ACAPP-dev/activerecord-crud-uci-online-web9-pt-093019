60d6925f4dc950f84760ee899e5c73521bdc485e
Andrew Capp <acapp909@gmail.com>
2020-01-02 23:38:20 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -13,9 +13,22 @@ end
 
 def can_be_created_with_a_hash_of_attributes
   # Initialize movie and then and save it
+  # Initialize movie and then and save it
+  attributes = {
+     title: "The Sting",
+     release_date: 1973,
+     director: "George Roy Hill",
+     lead: "Paul Newman",
+     in_theaters: false
+   }
+
+   movie = Movie.new
+   attributes.each do |key, value|
+     movie.send("#{key.to_s}=", value)
+   end
+   movie.save
+   movie
 end
-
-
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
