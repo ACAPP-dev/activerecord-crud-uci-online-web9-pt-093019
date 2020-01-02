db9f5e08f066aff6dc68df875365649a1706b47d
Andrew Capp <acapp909@gmail.com>
2020-01-02 23:52:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,7 +61,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  m = Movie.find_by(:title == "Title", :release_date == 2000, :director == "Me")
+  m = Movie.find_by(title: "Title", release_date: 2000, director: "Me")
   binding.pry
 end
 
